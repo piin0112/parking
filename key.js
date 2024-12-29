2956c608759d9356c250f04a3ca12b67b011383e
piin0112 <112914998+piin0112@users.noreply.github.com>
2024-12-29 16:14:13 +0800

    更新 park.scss 和 reset.scss 文件

---
+++ key.js
@@ -0,0 +1,5 @@
+const API_KEY = "AIzaSyBZTHvJBklpKp4lqsmArk3TOA2xar_T_6s"; // 請替換為實際的 API Key
+export default function key() {
+  return API_KEY;
+}
+<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&language=zh-TW&region=TW&callback=initMap" async defer></script>
